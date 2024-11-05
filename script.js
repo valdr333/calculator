88fa8ff3ca2cd6be09f6afea0f3192decbec310a
lav333 <val.dragasevic@gmail.com>
2024-11-05 17:41:25 +0100

    Update everything

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ buttons = document.querySelector(".buttons");
 let rows = [];
 for (let i = 1; i < 6; i++) {
     const div = document.createElement("div");
-    div.classList.add("row" + i);
-    div.classList.add("allRow");
+    div.classList.add("buttonsRow" + i);
+    div.classList.add("allRows");
     buttons.appendChild(div);
-    rows[i-1] = document.querySelector(".row"+i)
+    rows[i-1] = document.querySelector(".buttonsRow"+i)
 }
 
 for (let i = 0; i < 5; i++) {
@@ -33,6 +33,8 @@ for (let i = 0; i < 5; i++) {
         ac.textContent = "AC";
         const division = document.createElement("button");
         division.textContent = "÷";
+        ac.classList.add("buttonsRowButton", "buttonOther");
+        division.classList.add("buttonsRowButton", "buttonOperator");
         rows[i].appendChild(ac);
         rows[i].appendChild(division);
     } else if (i == 4) {
@@ -40,6 +42,8 @@ for (let i = 0; i < 5; i++) {
         nula.textContent = 0;
         const equal = document.createElement("button");
         equal.textContent = "=";
+        nula.classList.add("buttonsRowButton", "buttonNumber");
+        equal.classList.add("buttonsRowButton", "buttonOperator");
         rows[i].appendChild(nula);
         rows[i].appendChild(equal);
     } else {
@@ -47,6 +51,7 @@ for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 3; j++) {
             const button = document.createElement("button");
             button.textContent = 10-k;
+            button.classList.add("buttonsRowButton", "buttonNumber");
             rows[i].appendChild(button);
             k--;
         }
@@ -54,15 +59,21 @@ for (let i = 0; i < 5; i++) {
         if (i == 1) {
             const multiplication = document.createElement("button");
             multiplication.textContent = "x";
+            multiplication.classList.add("buttonsRowButton", "buttonOperator");
             rows[i].appendChild(multiplication);
         } else if (i == 2) {
             const subtraction = document.createElement("button");
             subtraction.textContent = "-";
+            subtraction.classList.add("buttonsRowButton", "buttonOperator");
             rows[i].appendChild(subtraction);
         } else if (i == 3) {
             const addition = document.createElement("button");
             addition.textContent = "+";
+            addition.classList.add("buttonsRowButton", "buttonOperator");
             rows[i].appendChild(addition);
         }
     }
 }
+
+display = document.querySelector(".display");
+display.textContent = "0123456789";
